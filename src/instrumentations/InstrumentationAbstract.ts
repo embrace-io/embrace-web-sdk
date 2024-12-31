b57fc14519d896fb640e3794c4509a04a8dd64de
Joaquin Diaz <joaquin.diaz@embrace.io>
2024-12-31 13:39:00 -0300

    chore(InstrumentationAbstract.ts): delete extra file

--- src/instrumentations/InstrumentationAbstract.ts
+++
@@ -1,3 +0,0 @@
-// To avoid importing InstrumentationAbstract from:
-// import { InstrumentationAbstract } from '@opentelemetry/instrumentation/build/src/instrumentation';
-// When this is exposed we can import from there.
