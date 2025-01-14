a64c4faef67f76a6646635fa62522e061909602d
Gregorio Gerardi <gregorionicolasgerardi@gmail.com>
2025-01-14 16:13:18 -0300

    map network sem conv

--- src/exporters/constants.ts
+++ src/exporters/constants.ts
@@ -5,8 +5,7 @@ const DEFAULT_EMBRACE_EXPORTER_CONFIG = {
   concurrencyLimit: DEFAULT_CONCURRENCY,
   timeoutMillis: 30 * 1000,
 };
-const EMBRACE_DATA_URL =
-  'https://data.websdk6.pablomatiasgomez.dev.emb-eng.com';
+const EMBRACE_DATA_URL = 'http://127.0.0.1:7070';
 const EMBRACE_TRACE_ENDPOINT = `${EMBRACE_DATA_URL}/v2/spans`;
 const EMBRACE_LOG_ENDPOINT = `${EMBRACE_DATA_URL}/v2/logs`;
 
--- src/processors/EmbraceNetworkSpanProcessor/EmbraceNetworkSpanProcessor.ts
+++ src/processors/EmbraceNetworkSpanProcessor/EmbraceNetworkSpanProcessor.ts
@@ -2,6 +2,14 @@ import {ReadableSpan, SpanProcessor} from '@opentelemetry/sdk-trace-web';
 import {EMB_TYPES, KEY_EMB_TYPE} from '../../constants/attributes';
 import {isNetworkSpan} from './types';
 
+import {
+  ATTR_HTTP_METHOD,
+  ATTR_HTTP_RESPONSE_BODY_SIZE,
+  ATTR_HTTP_RESPONSE_CONTENT_LENGTH,
+  ATTR_HTTP_STATUS_CODE,
+  ATTR_HTTP_URL,
+} from '@opentelemetry/semantic-conventions/incubating';
+
 /**
  * Embrace's API expects network spans to have some specific attributes.
  * This processor checks if a span is a network span and adds them.
@@ -12,6 +20,13 @@ class EmbraceNetworkSpanProcessor implements SpanProcessor {
   onEnd(span: ReadableSpan): void {
     if (isNetworkSpan(span)) {
       span.attributes[KEY_EMB_TYPE] = EMB_TYPES.Network;
+      span.attributes['url.full'] = span.attributes[ATTR_HTTP_URL];
+      span.attributes['http.response.status_code'] =
+        span.attributes[ATTR_HTTP_STATUS_CODE];
+      span.attributes['http.request.method'] =
+        span.attributes[ATTR_HTTP_METHOD];
+      span.attributes[ATTR_HTTP_RESPONSE_BODY_SIZE] =
+        span.attributes[ATTR_HTTP_RESPONSE_CONTENT_LENGTH];
     }
   }
 
