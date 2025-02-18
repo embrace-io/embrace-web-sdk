bf9debfe456ab1346e1c262421eefb23d95734db
Gregorio Gerardi <gregorionicolasgerardi@gmail.com>
2025-02-18 17:59:26 -0300

    rename our cli to web-cli

--- cli/src/constants.ts
+++ cli/src/constants.ts
@@ -1,6 +1,7 @@
 export const CLI_VERSION = '0.0.3';
-export const CLI_NAME = '@embraceio/sourcemaps-uploader';
-export const CLI_DESCRIPTION = 'Embrace Web SDK Source Uploader';
+export const CLI_NAME = '@embraceio/web-cli';
+export const CLI_DESCRIPTION =
+  'Embrace Web CLI to help setup the Embrace SDK in your web app';
 export const DEFAULT_FILE_ENCODING = 'utf8';
 export const SOURCE_MAP_UPLOAD_HOST = 'https://dsym-store.emb-api.com';
 export const SOURCE_MAP_UPLOAD_PATH = '/v2/store/';
