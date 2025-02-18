f5b8d3d3f3dab78010fae9ebc8c9cf4e7aac019a
Gregorio Gerardi <gregorionicolasgerardi@gmail.com>
2025-02-18 14:35:36 -0300

    Include SystemLog emb type for logs processed by our SDK

--- src/constants/attributes.ts
+++ src/constants/attributes.ts
@@ -5,6 +5,8 @@ export const KEY_JS_EXCEPTION_STACKTRACE = 'emb.stacktrace.js';
 export enum EMB_TYPES {
   Session = 'ux.session',
   Network = 'perf.network_request',
+  // SystemLog = 'sys.log', is an log emb type that tells the Embrace BE to process and keep the log.
+  // Our SDK adds it to any log generated through Embrace SDK.
   SystemLog = 'sys.log',
   WebVital = 'ux.web_vital',
 }
