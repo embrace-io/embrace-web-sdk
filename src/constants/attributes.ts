9367381e1bdbcea89c66711f4203c0c9cdfd3517
Gregorio Gerardi <gregorionicolasgerardi@gmail.com>
2025-02-19 09:16:10 -0300

    improve system log comment

--- src/constants/attributes.ts
+++ src/constants/attributes.ts
@@ -5,8 +5,7 @@ export const KEY_JS_EXCEPTION_STACKTRACE = 'emb.stacktrace.js';
 export enum EMB_TYPES {
   Session = 'ux.session',
   Network = 'perf.network_request',
-  // SystemLog = 'sys.log', is an log emb type that tells the Embrace BE to process and keep the log.
-  // Our SDK adds it to any log generated through Embrace SDK.
+  // SystemLog = 'sys.log', is a log emb type that tells the Embrace BE to treat this as an Embrace Log to be shown in the dashboard.
   SystemLog = 'sys.log',
   WebVital = 'ux.web_vital',
 }
