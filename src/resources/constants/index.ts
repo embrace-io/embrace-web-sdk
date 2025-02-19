eba7098735e0a18d66e45964c1bd5eaa21e8c2b6
Gregorio Gerardi <gregorionicolasgerardi@gmail.com>
2025-02-19 09:10:44 -0300

    rename WEB_FRAMEWORK to NATIVE_FRAMEWORK

--- src/resources/constants/index.ts
+++ src/resources/constants/index.ts
@@ -2,6 +2,6 @@
 // It needs to always remain 32 chars long, so we don't mess with the sourcemaps location after replacing the bundle id with the real 32 chars long bundle ID
 export const TEMPLATE_BUNDLE_ID = 'EmbIOBundleIDfd6996f1007b363f87a';
 export const EMBRACE_SERVICE_NAME = 'embrace-web-sdk';
-// WEB_FRAMEWORK is the representation Embrace BE uses to differentiate between
-// frameworks. It is an enum, so we just send a number here
-export const WEB_FRAMEWORK = 1;
+// NATIVE_FRAMEWORK is the representation Embrace BE uses to differentiate between
+// sdk . It is an enum, so we just send a number here
+export const NATIVE_FRAMEWORK = 1;
--- src/resources/webSdkResource.ts
+++ src/resources/webSdkResource.ts
@@ -3,15 +3,15 @@ import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 import { browserDetector } from '@opentelemetry/opentelemetry-browser-detector';
 import {
   EMBRACE_SERVICE_NAME,
+  NATIVE_FRAMEWORK,
   TEMPLATE_BUNDLE_ID,
-  WEB_FRAMEWORK,
 } from './constants/index.js';
 
 export const getWebSDKResource = () => {
   let resource = new Resource({
     [ATTR_SERVICE_NAME]: EMBRACE_SERVICE_NAME,
     app_version: '0.0.1', // TODO: this should be provided by the user / injected by our cli / both
-    app_framework: WEB_FRAMEWORK,
+    app_framework: NATIVE_FRAMEWORK,
     bundle_id: TEMPLATE_BUNDLE_ID,
     sdk_version: '0.0.1', // TODO: this should be injected by our cli
     sdk_simple_version: 1,
