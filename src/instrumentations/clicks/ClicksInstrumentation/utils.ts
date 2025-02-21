826c2f89a9e74730cf2e4e0125d9b290e0a2ae47
Jonathan Munz <jpmunz@gmail.com>
2025-02-21 17:09:39 -0500

    EMBR-6679 const

--- src/instrumentations/clicks/ClicksInstrumentation/utils.ts
+++ src/instrumentations/clicks/ClicksInstrumentation/utils.ts
@@ -1,6 +1,8 @@
+const MAX_INNER_TEXT = 30;
+
 export const getHTMLElementFriendlyName = (element: HTMLElement) => {
   const nodeName = element.nodeName.toLowerCase();
-  const innerText = element.innerText.substring(0, 50);
-  const ellipsis = element.innerText.length > 50 ? '...' : '';
+  const innerText = element.innerText.substring(0, MAX_INNER_TEXT);
+  const ellipsis = element.innerText.length > MAX_INNER_TEXT ? '...' : '';
   return `<${nodeName} class="${element.className}">${innerText}${ellipsis}</${nodeName}>`;
 };
