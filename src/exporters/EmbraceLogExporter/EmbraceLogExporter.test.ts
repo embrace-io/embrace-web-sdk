529b9861e5a376cddecc2078fc5774494b5fed2e
Gregorio Gerardi <gregorionicolasgerardi@gmail.com>
2025-03-19 13:28:18 -0300

    chore: update tests so we compare against JSON and not UInt8Array

--- src/exporters/EmbraceLogExporter/EmbraceLogExporter.test.ts
+++ src/exporters/EmbraceLogExporter/EmbraceLogExporter.test.ts
@@ -72,10 +72,10 @@ describe('EmbraceLogExporter', () => {
     expect((headers as Record<string, string>)['X-EM-DID']).to.equal(
       mockUserID
     );
-    // Chrome, Webkit and Firefox have slightly different encoding processes- Content-Length values. 182 for Chrome, 184 for Firefox and Webkit.
-    // TODO does our BE process both without issues?
+    // Chrome, Webkit and Firefox have slightly different encoding processes, generating different Content-Length values.
     const chromeContentLength = '189';
     const firefoxWebkitContentLength = '191';
+    //TODO we should find a way to know if we are running in Chrome, Firefox or Webkit and just assert for the specific value for each browser
     expect((headers as Record<string, string>)['Content-Length']).to.be.oneOf([
       chromeContentLength,
       firefoxWebkitContentLength
@@ -83,85 +83,43 @@ describe('EmbraceLogExporter', () => {
     expect(fakeFetch.getUrl()).to.equal(
       'https://a-testAppID.data.stg.emb-eng.com/v2/logs'
     );
-    // taken from an actual request
-    const encodedBodyChrome = new Uint8Array([
-      31, 139, 8, 0, 0, 0, 0, 0, 0, 19, 149, 143, 177, 10, 194, 64, 12, 134, 95,
-      69, 254, 249, 134, 186, 222, 38, 130, 32, 72, 5, 81, 23, 113, 104, 123,
-      161, 28, 182, 151, 146, 203, 21, 165, 244, 221, 165, 21, 234, 224, 36,
-      153, 242, 125, 225, 79, 50, 64, 40, 114, 146, 138, 14, 92, 71, 216, 219,
-      176, 0, 216, 1, 133, 170, 248, 50, 41, 77, 234, 110, 224, 132, 187, 142,
-      220, 102, 193, 91, 78, 65, 97, 179, 209, 32, 86, 220, 125, 83, 230, 110,
-      138, 8, 69, 75, 176, 80, 138, 138, 209, 160, 225, 250, 68, 21, 139, 251,
-      140, 169, 111, 233, 18, 252, 51, 47, 2, 195, 34, 131, 1, 151, 145, 164,
-      39, 119, 254, 85, 145, 122, 18, 175, 175, 60, 181, 37, 9, 236, 250, 139,
-      206, 244, 84, 88, 236, 243, 221, 17, 6, 37, 187, 215, 180, 60, 170, 248,
-      80, 95, 139, 38, 77, 55, 180, 92, 61, 86, 179, 26, 205, 31, 175, 221, 231,
-      122, 3, 192, 10, 83, 28, 42, 1, 0, 0
-    ]);
-    const encodedBodyFirefoxWebkit = new Uint8Array([
-      31, 139, 8, 0, 0, 0, 0, 0, 0, 19, 141, 143, 61, 11, 194, 64, 12, 134, 255,
-      138, 100, 238, 80, 215, 219, 68, 16, 4, 169, 32, 213, 69, 58, 220, 71, 40,
-      135, 189, 75, 185, 143, 210, 82, 252, 239, 222, 85, 104, 7, 23, 201, 16,
-      242, 190, 15, 201, 155, 25, 28, 122, 138, 78, 226, 133, 90, 15, 236, 57,
-      175, 2, 176, 25, 120, 8, 78, 139, 24, 48, 91, 77, 1, 202, 81, 223, 163,
-      58, 172, 242, 145, 162, 13, 192, 202, 119, 1, 94, 82, 191, 109, 89, 166,
-      188, 194, 114, 147, 58, 36, 54, 64, 162, 58, 106, 111, 40, 201, 169, 47,
-      22, 180, 193, 187, 213, 99, 197, 45, 37, 170, 132, 2, 72, 120, 116, 3,
-      170, 250, 215, 242, 56, 160, 211, 97, 170, 162, 17, 232, 128, 237, 55,
-      169, 198, 49, 229, 128, 115, 117, 186, 38, 80, 144, 154, 242, 113, 159,
-      114, 218, 246, 193, 187, 152, 51, 24, 146, 175, 221, 98, 165, 32, 255,
-      191, 214, 44, 245, 1, 192, 10, 83, 28, 42, 1, 0, 0
-    ]);
-    // Chrome, Webkit and Firefox have slightly different encoding processes. But all of them decode the actual body correctly as the below object
-    /*
-    {
-      "resourceLogs": [
-          {
-              "resource": {
-                  "attributes": [],
-                  "droppedAttributesCount": 0
-              },
-              "scopeLogs": [
-                  {
-                      "scope": {},
-                      "logRecords": [
-                          {
-                              "timeUnixNano": "0",
-                              "observedTimeUnixNano": "0",
-                              "severityNumber": 1,
-                              "severityText": "INFO",
-                              "body": {
-                                  "stringValue": "mock body"
-                              },
-                              "attributes": [],
-                              "droppedAttributesCount": 0
-                          }
-                      ]
-                  }
-              ]
-          }
-      ]
-    }
-     */
-    // to validate what the encoded body translates to, or compare different browsers,
-    // uncomment the following lines and check the console using npm run
-    // sdk:test:manual from different browsers
-    // const blob = new Blob([fakeFetch.getBody() as Uint8Array]);
-    // const decompressedStream = new DecompressionStream('gzip');
-    // const decompressedBlob = blob.stream().pipeThrough(decompressedStream);
-    // const text = await new Response(decompressedBlob).text();
-    // console.log(JSON.parse(text));
     const body = fakeFetch.getBody();
     void expect(body).not.to.be.null;
-    const encodedBodyString = JSON.stringify(fakeFetch.getBody());
-    const expectedBodyChromeString = JSON.stringify(encodedBodyChrome);
-    const expectedBodyFirefoxWebkitString = JSON.stringify(
-      encodedBodyFirefoxWebkit
+    // assert that the decompressed and decoded body is the expected one
+    const expectedBody = {
+      resourceLogs: [
+        {
+          resource: {
+            attributes: [],
+            droppedAttributesCount: 0
+          },
+          scopeLogs: [
+            {
+              scope: { name: 'test' },
+              logRecords: [
+                {
+                  timeUnixNano: '0',
+                  observedTimeUnixNano: '0',
+                  severityNumber: 1,
+                  severityText: 'INFO',
+                  body: {
+                    stringValue: 'mock body'
+                  },
+                  attributes: [],
+                  droppedAttributesCount: 0
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    };
+    const decompressedStream = new Response(body).body?.pipeThrough(
+      new DecompressionStream('gzip')
     );
-    //TODO we should find a way to know if we are running in Chrome, Firefox or Webkit and just assert for the specific value for each browser
-    expect(encodedBodyString).to.be.oneOf([
-      expectedBodyChromeString,
-      expectedBodyFirefoxWebkitString
-    ]);
+    // translate from Uint8Array to string
+    const text = await new Response(decompressedStream).text();
+    const parsed = JSON.parse(text) as never;
+    expect(parsed).to.deep.equal(expectedBody);
   });
 });
--- src/exporters/EmbraceTraceExporter/EmbraceTraceExporter.test.ts
+++ src/exporters/EmbraceTraceExporter/EmbraceTraceExporter.test.ts
@@ -84,9 +84,9 @@ describe('EmbraceTraceExporter', () => {
       mockUserID
     );
     // Chrome, Webkit and Firefox have slightly different encoding processes- Content-Length values. 182 for Chrome, 184 for Firefox and Webkit.
-    // TODO does our BE process both without issues?
     const chromeContentLength = '203';
     const firefoxWebkitContentLength = '205';
+    //TODO we should find a way to know if we are running in Chrome, Firefox or Webkit and just assert for the specific value for each browser
     expect((headers as Record<string, string>)['Content-Length']).to.be.oneOf([
       chromeContentLength,
       firefoxWebkitContentLength
@@ -94,96 +94,52 @@ describe('EmbraceTraceExporter', () => {
     expect(fakeFetch.getUrl()).to.equal(
       'https://a-testAppID.data.stg.emb-eng.com/v2/spans'
     );
-    // taken from an actual request
-    const encodedBodyChrome = new Uint8Array([
-      31, 139, 8, 0, 0, 0, 0, 0, 0, 19, 141, 144, 177, 10, 2, 49, 16, 68, 127,
-      69, 166, 222, 66, 45, 211, 137, 88, 8, 98, 163, 86, 98, 17, 147, 45, 194,
-      121, 155, 35, 217, 19, 225, 240, 223, 37, 17, 79, 5, 11, 217, 106, 223,
-      204, 14, 204, 14, 72, 156, 99, 159, 28, 239, 58, 43, 25, 230, 56, 140, 4,
-      102, 128, 85, 77, 225, 220, 43, 23, 233, 68, 240, 41, 118, 29, 251, 197,
-      136, 151, 177, 23, 133, 153, 222, 9, 217, 197, 238, 35, 166, 174, 37, 67,
-      108, 203, 48, 80, 206, 10, 194, 149, 83, 14, 81, 96, 48, 67, 57, 26, 253,
-      154, 172, 227, 181, 175, 194, 147, 215, 101, 14, 122, 37, 180, 209, 53,
-      147, 34, 128, 208, 4, 241, 48, 115, 66, 86, 155, 116, 31, 90, 62, 72, 184,
-      109, 173, 68, 24, 76, 65, 96, 241, 63, 232, 223, 125, 8, 124, 101, 209,
-      111, 219, 170, 162, 183, 37, 171, 213, 62, 151, 142, 46, 122, 134, 153,
-      221, 9, 151, 32, 205, 247, 213, 166, 144, 241, 79, 167, 58, 15, 201, 171,
-      118, 165, 120, 1, 0, 0
-    ]);
-    const encodedBodyFirefoxWebkit = new Uint8Array([
-      31, 139, 8, 0, 0, 0, 0, 0, 0, 19, 141, 144, 49, 11, 194, 48, 16, 133, 255,
-      138, 220, 220, 161, 237, 216, 77, 196, 65, 16, 23, 117, 146, 14, 49, 185,
-      33, 212, 94, 74, 114, 41, 66, 241, 191, 123, 137, 82, 45, 56, 72, 134,
-      112, 223, 123, 247, 46, 151, 9, 60, 6, 23, 189, 198, 227, 160, 40, 64,
-      115, 153, 102, 2, 205, 4, 138, 217, 219, 107, 100, 76, 82, 91, 128, 241,
-      110, 24, 208, 172, 103, 188, 113, 145, 24, 154, 242, 81, 64, 208, 110,
-      248, 138, 201, 101, 202, 32, 213, 203, 13, 98, 102, 40, 96, 68, 31, 172,
-      35, 1, 21, 164, 166, 217, 207, 94, 105, 220, 153, 44, 188, 120, 46, 106,
-      41, 222, 9, 189, 211, 221, 42, 9, 130, 58, 75, 162, 214, 98, 100, 229,
-      249, 100, 123, 60, 147, 189, 31, 20, 57, 49, 150, 98, 64, 50, 63, 232,
-      223, 251, 72, 192, 136, 196, 75, 219, 54, 163, 143, 69, 102, 115, 12, 105,
-      71, 237, 140, 188, 176, 146, 133, 110, 150, 186, 101, 215, 62, 145, 249,
-      159, 218, 124, 158, 201, 171, 118, 165, 120, 1, 0, 0
-    ]);
-    // Chrome, Webkit and Firefox have slightly different encoding processes. But all of them decode the actual body correctly as the below object
-    /*
-    {
-      "resourceSpans": [
+    // assert that the decompressed and decoded body is the expected one
+    const body = fakeFetch.getBody();
+    void expect(body).not.to.be.null;
+    const expectedBody = {
+      resourceSpans: [
         {
-          "resource": {
-            "attributes": [],
-            "droppedAttributesCount": 0
+          resource: {
+            attributes: [],
+            droppedAttributesCount: 0
           },
-          "scopeSpans": [
+          scopeSpans: [
             {
-              "scope": {
-                "name": "test",
-                "version": "1"
+              scope: {
+                name: 'test',
+                version: '1'
               },
-              "spans": [
+              spans: [
                 {
-                  "traceId": "1",
-                  "spanId": "2",
-                  "name": "mock span",
-                  "kind": 2,
-                  "startTimeUnixNano": "0",
-                  "endTimeUnixNano": "0",
-                  "attributes": [],
-                  "droppedAttributesCount": 0,
-                  "events": [],
-                  "droppedEventsCount": 0,
-                  "status": {
-                    "code": 1
+                  traceId: '1',
+                  spanId: '2',
+                  name: 'mock span',
+                  kind: 2,
+                  startTimeUnixNano: '0',
+                  endTimeUnixNano: '0',
+                  attributes: [],
+                  droppedAttributesCount: 0,
+                  events: [],
+                  droppedEventsCount: 0,
+                  status: {
+                    code: 1
                   },
-                  "links": [],
-                  "droppedLinksCount": 0
+                  links: [],
+                  droppedLinksCount: 0
                 }
               ]
             }
           ]
         }
       ]
-    }
-     */
-    // to validate what the encoded body translates to, or compare different browsers,
-    // uncomment the following lines and check the console using npm run
-    // sdk:test:manual from different browsers
-    // const blob = new Blob([fakeFetch.getBody() as Uint8Array]);
-    // const decompressedStream = new DecompressionStream('gzip');
-    // const decompressedBlob = blob.stream().pipeThrough(decompressedStream);
-    // const text = await new Response(decompressedBlob).text();
-    // console.log(JSON.parse(text));
-    const body = fakeFetch.getBody();
-    void expect(body).not.to.be.null;
-    const encodedBodyString = JSON.stringify(fakeFetch.getBody());
-    const expectedBodyChromeString = JSON.stringify(encodedBodyChrome);
-    const expectedBodyFirefoxWebkitString = JSON.stringify(
-      encodedBodyFirefoxWebkit
+    };
+    const decompressedStream = new Response(body).body?.pipeThrough(
+      new DecompressionStream('gzip')
     );
-    //TODO we should find a way to know if we are running in Chrome, Firefox or Webkit and just assert for the specific value for each browser
-    expect(encodedBodyString).to.be.oneOf([
-      expectedBodyChromeString,
-      expectedBodyFirefoxWebkitString
-    ]);
+    // translate from Uint8Array to string
+    const text = await new Response(decompressedStream).text();
+    const parsed = JSON.parse(text) as never;
+    expect(parsed).to.deep.equal(expectedBody);
   });
 });
