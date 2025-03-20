cc75ad9427c47ae283b7e684afd4edab174d5323
Gregorio Gerardi <gregorionicolasgerardi@gmail.com>
2025-03-20 14:09:27 -0300

    chore: shorten all imports from initSDK to max one folder, add intermediate exports (#128)
    Shortening imports. We had been forgetting to reexport from intermediate packages. No import should ever include more than one folder max

--- src/api-users/index.ts
+++ src/api-users/index.ts
@@ -1,3 +1,7 @@
 export { user } from './userAPI.js';
 export type { UserManager } from './manager/index.js';
-export { NoOpUserManager, ProxyUserManager } from './manager/index.js';
+export {
+  NoOpUserManager,
+  ProxyUserManager,
+  KEY_ENDUSER_PSEUDO_ID
+} from './manager/index.js';
