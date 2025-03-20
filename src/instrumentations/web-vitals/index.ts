7df306e6de0559317bae352051c83ec7420c92e4
Gregorio Gerardi <gregorionicolasgerardi@gmail.com>
2025-03-20 14:06:24 -0300

    chore: remove unnecessary exports from src/instrumentations (#127)
    ### TL;DR
    
    Reduced API surface by removing unnecessary exports from web-vitals and session instrumentations.

--- src/instrumentations/web-vitals/index.ts
+++ src/instrumentations/web-vitals/index.ts
@@ -1,9 +1,1 @@
-export {
-  WebVitalsInstrumentation,
-  EMB_WEB_VITALS_PREFIX,
-  METER_NAME,
-  CORE_WEB_VITALS,
-  NOT_CORE_WEB_VITALS,
-  WEB_VITALS,
-  WEB_VITALS_ID_TO_LISTENER
-} from './WebVitalsInstrumentation/index.js';
+export { WebVitalsInstrumentation } from './WebVitalsInstrumentation/index.js';
