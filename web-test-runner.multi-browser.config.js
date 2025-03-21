f0ec27ae1dec25cb7b7aa3518953dea16a6b1ad5
Gregorio Gerardi <gregorionicolasgerardi@gmail.com>
2025-03-21 11:06:48 -0300

    fix: give multi browser tests more time to start and just run them on main

--- web-test-runner.multi-browser.config.js
+++ web-test-runner.multi-browser.config.js
@@ -1,24 +1,36 @@
 import { devices, playwrightLauncher } from '@web/test-runner-playwright';
 import baseConfig from './web-test-runner.config.js';
 
+const TEN_MINUTES = 600000;
+
 export default {
   ...baseConfig,
+  browserStartTimeout: TEN_MINUTES, // 10 minutes. Required as we do not parallelize tests enough to start all browsers at once.
   browsers: [
     /* Test against desktop browsers */
     playwrightLauncher({
       product: 'chromium',
+      launchOptions: {
+        timeout: TEN_MINUTES
+      },
       createBrowserContext({ browser }) {
         return browser.newContext({ ...devices['Desktop Chrome'] });
       }
     }),
     playwrightLauncher({
       product: 'firefox',
+      launchOptions: {
+        timeout: TEN_MINUTES
+      },
       createBrowserContext({ browser }) {
         return browser.newContext({ ...devices['Desktop Firefox'] });
       }
     }),
     playwrightLauncher({
       product: 'webkit',
+      launchOptions: {
+        timeout: TEN_MINUTES
+      },
       createBrowserContext({ browser }) {
         return browser.newContext({ ...devices['Desktop Safari'] });
       }
@@ -26,12 +38,18 @@ export default {
     /* Test against mobile browsers */
     playwrightLauncher({
       product: 'chromium',
+      launchOptions: {
+        timeout: TEN_MINUTES
+      },
       createBrowserContext({ browser }) {
         return browser.newContext({ ...devices['Pixel 5'] });
       }
     }),
     playwrightLauncher({
       product: 'webkit',
+      launchOptions: {
+        timeout: TEN_MINUTES
+      },
       createBrowserContext({ browser }) {
         return browser.newContext({ ...devices['iPhone 12'] });
       }
@@ -39,6 +57,9 @@ export default {
     /* Test against branded browsers. */
     playwrightLauncher({
       product: 'chromium',
+      launchOptions: {
+        timeout: TEN_MINUTES
+      },
       createBrowserContext({ browser }) {
         return browser.newContext({
           ...devices['Desktop Chrome'],
@@ -48,6 +69,9 @@ export default {
     }),
     playwrightLauncher({
       product: 'chromium',
+      launchOptions: {
+        timeout: TEN_MINUTES
+      },
       createBrowserContext({ browser }) {
         return browser.newContext({
           ...devices['Desktop Edge'],
