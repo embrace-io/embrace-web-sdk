3ff540eeda5b4e601a332499f5777d6034890039
Jonathan Munz <jpmunz@gmail.com>
2025-03-31 14:48:27 -0400

    test: remaining processors

---
+++ src/processors/EmbraceNetworkSpanProcessor/EmbraceNetworkSpanProcessor.test.ts
@@ -0,0 +1,123 @@
+import * as chai from 'chai';
+import type { InMemorySpanExporter } from '@opentelemetry/sdk-trace-web';
+import { setupTestTraceExporter } from '../../testUtils/setupTestTraceExporter/setupTestTraceExporter.js';
+import type { Tracer } from '@opentelemetry/api';
+import { trace } from '@opentelemetry/api';
+import { EmbraceNetworkSpanProcessor } from './EmbraceNetworkSpanProcessor.js';
+
+const { expect } = chai;
+
+describe('EmbraceNetworkSpanProcessor', () => {
+  let memoryExporter: InMemorySpanExporter;
+  let tracer: Tracer;
+
+  before(() => {
+    memoryExporter = setupTestTraceExporter([
+      new EmbraceNetworkSpanProcessor(),
+    ]);
+    tracer = trace.getTracer('test-tracer');
+  });
+
+  afterEach(() => {
+    memoryExporter.reset();
+  });
+
+  it('should add emb.type when the span represents a network request', () => {
+    tracer
+      .startSpan('network-request', {
+        attributes: {
+          'http.request.method': 'GET',
+          'http.response.status_code': 200,
+          'http.response.body.size': 10,
+          'http.request.body.size': 20,
+          'url.full': 'https://example.com',
+        },
+      })
+      .end();
+
+    const finishedSpans = memoryExporter.getFinishedSpans();
+    expect(finishedSpans).to.have.lengthOf(1);
+    const networkRequest = finishedSpans[0];
+    expect(networkRequest.attributes).to.be.deep.equal({
+      'emb.type': 'perf.network_request',
+      'http.request.method': 'GET',
+      'http.response.status_code': 200,
+      'http.response.body.size': 10,
+      'http.request.body.size': 20,
+      'url.full': 'https://example.com',
+    });
+  });
+
+  it('should copy over deprecated network attributes', () => {
+    tracer
+      .startSpan('network-request', {
+        attributes: {
+          'http.method': 'GET',
+          'http.status_code': 200,
+          'http.response_content_length': 10,
+          'http.request_content_length': 20,
+          'http.url': 'https://example.com',
+        },
+      })
+      .end();
+
+    const finishedSpans = memoryExporter.getFinishedSpans();
+    expect(finishedSpans).to.have.lengthOf(1);
+    const networkRequest = finishedSpans[0];
+    expect(networkRequest.attributes).to.be.deep.equal({
+      'emb.type': 'perf.network_request',
+      'http.request.method': 'GET',
+      'http.response.status_code': 200,
+      'http.response.body.size': 10,
+      'http.request.body.size': 20,
+      'url.full': 'https://example.com',
+      'http.method': 'GET',
+      'http.status_code': 200,
+      'http.response_content_length': 10,
+      'http.request_content_length': 20,
+      'http.url': 'https://example.com',
+    });
+  });
+
+  it('should do nothing for a non-network span', () => {
+    tracer
+      .startSpan('some-span', {
+        attributes: {
+          foo: 'bar',
+        },
+      })
+      .end();
+
+    const finishedSpans = memoryExporter.getFinishedSpans();
+    expect(finishedSpans).to.have.lengthOf(1);
+    const networkRequest = finishedSpans[0];
+    expect(networkRequest.attributes).to.be.deep.equal({
+      foo: 'bar',
+    });
+  });
+
+  it('should do nothing if the network span does not have a valid url', () => {
+    tracer
+      .startSpan('network-request', {
+        attributes: {
+          'http.request.method': 'GET',
+          'http.response.status_code': 200,
+          'http.response.body.size': 10,
+          'http.request.body.size': 20,
+          'url.full': '/some/path',
+        },
+      })
+      .end();
+
+    const finishedSpans = memoryExporter.getFinishedSpans();
+    expect(finishedSpans).to.have.lengthOf(1);
+    const networkRequest = finishedSpans[0];
+    expect(networkRequest.attributes).to.be.deep.equal({
+      'http.request.method': 'GET',
+      'http.response.status_code': 200,
+      'http.response.body.size': 10,
+      'http.request.body.size': 20,
+      'url.full': '/some/path',
+    });
+  });
+});
--- src/testUtils/setupTestLogExporter/setupTestLogExporter.ts
+++ src/testUtils/setupTestLogExporter/setupTestLogExporter.ts
@@ -1,4 +1,5 @@
 import { logs } from '@opentelemetry/api-logs';
+import type { LogRecordProcessor } from '@opentelemetry/sdk-logs';
 import {
   InMemoryLogRecordExporter,
   LoggerProvider,
@@ -9,9 +10,14 @@ import {
  * setupTestLogExporter is a utility function that sets up a test log exporter for use in testing.
  * It returns an instance of InMemoryLogRecordExporter, hooked into a SimpleLogRecordProcessor, and a LoggerProvider.
  * */
-export const setupTestLogExporter = () => {
+export const setupTestLogExporter = (
+  logProcessors: LogRecordProcessor[] = []
+) => {
   const memoryExporter = new InMemoryLogRecordExporter();
   const logProvider = new LoggerProvider();
+  logProcessors.forEach(processor => {
+    logProvider.addLogRecordProcessor(processor);
+  });
   logProvider.addLogRecordProcessor(
     new SimpleLogRecordProcessor(memoryExporter)
   );
