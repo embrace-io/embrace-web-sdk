093e2d274601ab11da7c42d41b346af37d4d8ca7
Jonathan Munz <jpmunz@gmail.com>
2025-03-31 16:55:28 -0400

    add comment

--- src/testUtils/MockPerformanceManager/MockPerformanceManager.ts
+++ src/testUtils/MockPerformanceManager/MockPerformanceManager.ts
@@ -4,6 +4,8 @@ export class MockPerformanceManager extends OTelPerformanceManager {
   public constructor(clock: sinon.SinonFakeTimers) {
     super({
       now: () => clock.now,
+      // For real performance timing `timeOrigin` is the timestamp when the document's lifetime started: https://developer.mozilla.org/en-US/docs/Web/API/Performance/timeOrigin#value
+      // When mocking set it to whatever value our fake clock had at the time that this MockPerformanceManager was instantiated
       timeOrigin: clock.now,
     });
   }
