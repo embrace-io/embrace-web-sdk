8079be4371f1de51c25c5f6cdb7b829fadcf0e78
Gregorio Gerardi <gregorionicolasgerardi@gmail.com>
2025-04-01 16:35:43 -0700

    fix: exclude OTel and test utility files from coverage

--- web-test-runner.coverage.config.js
+++ web-test-runner.coverage.config.js
@@ -4,8 +4,14 @@ export default {
   ...baseConfig,
   coverage: true,
   coverageConfig: {
-    include: ['src/**/*.ts'],
-    report: true,
     reporters: ['cobertura'],
+    include: ['src/**/*.ts'],
+    exclude: [
+      // no need to account for coverage for testing helpers
+      'src/testUtils/**/*.ts',
+      // no need to test InstrumentationAbstract as we just copied from OTel. They tested it lol
+      'src/instrumentations/InstrumentationAbstract/**/*.ts',
+    ],
+    report: true,
   },
 };
