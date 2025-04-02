0115953556fe1a5a9c9b28bfe6c1fa033db123eb
Gregorio Gerardi <gregorionicolasgerardi@gmail.com>
2025-04-02 14:21:56 -0700

    chore: only report files under 'src' while computing testing coverage (#172)

--- web-test-runner.coverage.config.js
+++ web-test-runner.coverage.config.js
@@ -4,7 +4,12 @@ export default {
   ...baseConfig,
   coverage: true,
   coverageConfig: {
+    reporters: ['cobertura'],
+    include: ['src/**/*.ts'],
+    exclude: [
+      // no need to account for coverage for testing helpers
+      'src/testUtils/**/*.ts',
+    ],
     report: true,
-    reporters: ['cobertura']
-  }
+  },
 };
