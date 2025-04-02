2bf6c4d23db8f0110e29032de0cae1699b82815f
Gregorio Gerardi <gregorionicolasgerardi@gmail.com>
2025-04-02 11:05:45 -0700

    fix: add global testing import to keep files with no coverage in the report

--- web-test-runner.coverage.config.js
+++ web-test-runner.coverage.config.js
@@ -9,8 +9,6 @@ export default {
     exclude: [
       // no need to account for coverage for testing helpers
       'src/testUtils/**/*.ts',
-      // no need to test InstrumentationAbstract as we just copied from OTel. They tested it lol
-      'src/instrumentations/InstrumentationAbstract/**/*.ts',
     ],
     report: true,
   },
