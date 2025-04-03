d1a3359417f82d96ab4a2b7d92bbf0bf5ef3e371
Gregorio Gerardi <gregorionicolasgerardi@gmail.com>
2025-04-03 11:10:27 -0700

    chore: add otel copyright comments (#180)
    (AI generated) 
    ### TL;DR
    
    Added OpenTelemetry license headers to instrumentation files.
    
    ### What changed?
    
    Added the Apache 2.0 license headers from OpenTelemetry to three instrumentation files:
    - `EmbraceInstrumentationBase.ts`
    - `InstrumentationAbstract.ts`
    - `InstrumentationBase.ts`
    
    These headers properly attribute the code that was copied from the OpenTelemetry project.
    
    ### How to test?
    
    No functional testing is required as this is a documentation change. Verify that the license headers are correctly formatted and placed in the appropriate files.
    
    ### Why make this change?
    
    This change ensures proper attribution and license compliance for code copied from the OpenTelemetry project. The added headers clarify that portions of the code are subject to the Apache 2.0 license as required by the OpenTelemetry authors.

--- src/instrumentations/InstrumentationAbstract/InstrumentationAbstract.ts
+++ src/instrumentations/InstrumentationAbstract/InstrumentationAbstract.ts
@@ -20,6 +20,21 @@ import * as shimmer from 'shimmer';
 
 // copied directly from https://github.com/open-telemetry/opentelemetry-js/blob/90afa2850c0690f7a18ecc511c04927a3183490b/experimental/packages/opentelemetry-instrumentation/src/instrumentation.ts
 // to avoid importing internal and experimental code.
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 export abstract class InstrumentationAbstract<
   ConfigType extends InstrumentationConfig = InstrumentationConfig,
 > implements Instrumentation<ConfigType>
--- src/instrumentations/InstrumentationBase/InstrumentationBase.ts
+++ src/instrumentations/InstrumentationBase/InstrumentationBase.ts
@@ -7,6 +7,23 @@ import { InstrumentationAbstract } from '../InstrumentationAbstract/index.js';
 
 // copied directly from https://github.com/open-telemetry/opentelemetry-js/blob/90afa2850c0690f7a18ecc511c04927a3183490b/experimental/packages/opentelemetry-instrumentation/src/platform/browser/instrumentation.ts
 // to avoid importing internal and experimental code.
+
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 export abstract class InstrumentationBase<
     ConfigType extends InstrumentationConfig = InstrumentationConfig,
   >
