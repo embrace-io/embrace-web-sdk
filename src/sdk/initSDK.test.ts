3450a99345717b6a951d7da7d36db077c82bc0b1
Jonathan Munz <jpmunz@gmail.com>
2025-04-10 13:34:07 -0400

    EMBR-7161 add test for Embrace export

--- src/sdk/initSDK.test.ts
+++ src/sdk/initSDK.test.ts
@@ -8,6 +8,11 @@ import {
   FakeLogRecordProcessor,
   FakeSpanProcessor,
   setupTestWebVitalListeners,
+  fakeFetchInstall,
+  fakeFetchRestore,
+  fakeFetchRespondWith,
+  fakeFetchGetRequestHeaders,
+  fakeFetchGetBody,
 } from '../testUtils/index.js';
 import { trace } from '@opentelemetry/api';
 import { logs } from '@opentelemetry/api-logs';
@@ -15,6 +20,8 @@ import type { WebVitalOnReport } from '../instrumentations/web-vitals/WebVitalsI
 import type { MetricWithAttribution } from 'web-vitals/attribution';
 import sinonChai from 'sinon-chai';
 import { session } from '../api-sessions/index.js';
+import { Resource } from '@opentelemetry/resources';
+import { SDK_VERSION } from '../resources/constants/index.js';
 
 chai.use(sinonChai);
 const { expect } = chai;
@@ -163,5 +170,92 @@ describe('initSDK', () => {
     });
     void expect(result).not.to.be.false;
     void expect(testWebVitalListeners.clsStub).not.to.have.been.called;
+  });
+
+  describe('export to Embrace', () => {
+    beforeEach(() => {
+      fakeFetchInstall();
+    });
+
+    afterEach(() => {
+      fakeFetchRestore();
+    });
+
+    it('should include the correct resource attributes', async () => {
+      fakeFetchRespondWith('');
+      const result = initSDK({
+        appID: 'abc12',
+        appVersion: 'my-app-version',
+        resource: new Resource({ r1: 'my-resource-attr' }),
+        defaultInstrumentationConfig: {
+          // This instrumentation does its own patching of Fetch which interferes with our test stub
+          omit: new Set(['@opentelemetry/instrumentation-fetch']),
+        },
+      });
+      void expect(result).not.to.be.false;
+
+      const sessionID = session.getSpanSessionManager().getSessionId();
+      session.getSpanSessionManager().endSessionSpan();
+
+      // Needed to allow the transport to actually send its data off to fetch
+      await new Promise(r => setTimeout(r, 1));
+
+      const headers = fakeFetchGetRequestHeaders();
+      expect((headers as Record<string, string>)['X-EM-AID']).to.equal('abc12');
+
+      const body = fakeFetchGetBody();
+      void expect(body).not.to.be.null;
+      const decompressedStream = new Response(body).body?.pipeThrough(
+        new DecompressionStream('gzip')
+      );
+      // translate from Uint8Array to string
+      const text = await new Response(decompressedStream).text();
+      const parsed = JSON.parse(text) as never;
+
+      expect(parsed['resourceSpans']).to.have.lengthOf(1);
+      const resourceSpan = parsed['resourceSpans'][0];
+      const resource = resourceSpan['resource'];
+      expect(resource).to.deep.equal({
+        attributes: [
+          { key: 'service.name', value: { stringValue: 'embrace-web-sdk' } },
+          { key: 'telemetry.sdk.language', value: { stringValue: 'webjs' } },
+          {
+            key: 'telemetry.sdk.name',
+            value: { stringValue: 'opentelemetry' },
+          },
+          { key: 'telemetry.sdk.version', value: { stringValue: '1.30.0' } },
+          { key: 'r1', value: { stringValue: 'my-resource-attr' } },
+          { key: 'app_version', value: { stringValue: 'my-app-version' } },
+          { key: 'app_framework', value: { intValue: 1 } },
+          {
+            key: 'bundle_id',
+            value: { stringValue: 'EmbIOBundleIDfd6996f1007b363f87a' },
+          },
+          { key: 'sdk_version', value: { stringValue: SDK_VERSION } },
+          { key: 'sdk_simple_version', value: { intValue: 1 } },
+          { key: 'sdk_platform', value: { stringValue: 'web' } },
+        ],
+        droppedAttributesCount: 0,
+      });
+
+      void expect(resourceSpan['scopeSpans']).not.to.be.undefined;
+      expect(resourceSpan['scopeSpans']).to.have.lengthOf(1);
+      const scopeSpan = resourceSpan['scopeSpans'][0];
+      expect(scopeSpan['scope']).to.deep.equal({
+        name: 'embrace-web-sdk-sessions',
+      });
+      expect(scopeSpan['spans']).to.have.lengthOf(1);
+      const sessionSpan = scopeSpan['spans'][0];
+      expect(sessionSpan['name']).to.be.equal('emb-session');
+      expect(sessionSpan['attributes']).to.deep.equal([
+        { key: 'emb.type', value: { stringValue: 'ux.session' } },
+        { key: 'emb.state', value: { stringValue: 'foreground' } },
+        {
+          key: 'session.id',
+          value: { stringValue: sessionID },
+        },
+        { key: 'emb.session_end_type', value: { stringValue: 'manual' } },
+      ]);
+    });
   });
 });
