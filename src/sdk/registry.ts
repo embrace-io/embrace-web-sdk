03acd88f9e76e44c04bf95781834077bc17bb973
Jonathan Munz <jpmunz@gmail.com>
2025-04-15 09:33:30 -0400

    chore: add diagnostic logging to the registry

--- src/sdk/registry.ts
+++ src/sdk/registry.ts
@@ -1,13 +1,28 @@
 import type { SDKControl, SDKRegistryManager } from './types.js';
+import type { DiagLogger } from '@opentelemetry/api';
+import { diag } from '@opentelemetry/api';
 
 class Registry implements SDKRegistryManager {
   private _sdk: SDKControl | null = null;
+  private readonly _diag: DiagLogger;
+
+  public constructor({
+    diagLogger = diag.createComponentLogger({ namespace: 'embrace-registry' }),
+  }: { diagLogger?: DiagLogger } = {}) {
+    this._diag = diagLogger;
+  }
 
   public register: (sdk: SDKControl) => void = sdk => {
+    if (this._sdk !== null) {
+      this._diag.warn('previously registered sdk will be overwritten');
+    }
     this._sdk = sdk;
   };
 
   public clear: () => void = () => {
+    if (this._sdk === null) {
+      this._diag.warn('sdk already cleared, this is a no-op');
+    }
     this._sdk = null;
   };
 
