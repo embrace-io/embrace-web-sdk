22a38c33f2c38184d4e6ba864147ea8b55040aa7
Jonathan Munz <jpmunz@gmail.com>
2025-05-07 16:32:58 -0400

    chore: works

---
+++ demo/webpack-example/src/index.ts
@@ -0,0 +1,14 @@
+import { sdk } from '@embrace-io/web-sdk';
+
+const result = sdk.initSDK({
+  appID: 'abc12',
+  appVersion: 'YOUR_APP_VERSION',
+});
+
+if (!!result) {
+  console.log('Successfully initialized the Embrace SDK');
+} else {
+  console.log('Failed to initialize the Embrace SDK');
+}
+
+console.log('Hello This is a Typescript project.');
