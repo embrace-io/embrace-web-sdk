ca797ae720837af74adbebaa67b453cf4b4e5eb5
Joaquin Diaz <joaquin.diaz@embrace.io>
2025-06-02 14:01:45 -0300

    chore: clean up after conflicts

--- demo/frontend/src/main.tsx
+++ demo/frontend/src/main.tsx
@@ -3,27 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { setupOTel } from './otel.js';
-import { createBrowserHistory } from 'history';
-import { Route, Router, Switch } from 'react-router-dom';
-import About from './About';
-import { withEmbraceRouting } from '@embrace-io/web-sdk/react-instrumentation';
-
-const history = createBrowserHistory();
-const OTelRoute = withEmbraceRouting(Route);
 
 setupOTel();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router history={history}>
-      <Switch>
-        <OTelRoute exact path="/">
-          <App />
-        </OTelRoute>
-        <OTelRoute path="/about/:id">
-          <About />
-        </OTelRoute>
-      </Switch>
-    </Router>
+    <App />
   </StrictMode>
 );
