c5063edbe497e4565b56c48642c07727163e0291
Joaquin Diaz <joaquin.diaz@embrace.io>
2025-06-06 10:15:04 -0300

    chore: add tests for ProxyTraceManager

--- src/api-traces/manager/ProxyTraceManager/ProxyTraceManager.test.ts
+++ src/api-traces/manager/ProxyTraceManager/ProxyTraceManager.test.ts
@@ -1,10 +1,11 @@
-import type { Span } from '@opentelemetry/api';
+import type { Context, Span } from '@opentelemetry/api';
 import * as chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import type { TraceManager } from '../index.js';
 import { NoOpTraceManager } from '../NoOpTraceManager/index.js';
 import { ProxyTraceManager } from './ProxyTraceManager.js';
+import type { ExtendedSpan } from '../../api/index.js';
 
 chai.use(sinonChai);
 const { expect } = chai;
@@ -17,6 +18,8 @@ describe('ProxyTraceManager', () => {
     proxyTraceManager = new ProxyTraceManager();
     mockDelegate = {
       startSpan: sinon.stub().returns({} as Span),
+      setSpan: sinon.stub().returns({} as Context),
+      getSpan: sinon.stub().returns(undefined),
     };
   });
 
@@ -35,5 +38,20 @@ describe('ProxyTraceManager', () => {
     proxyTraceManager.setDelegate(mockDelegate);
     const span = proxyTraceManager.startSpan('span-name');
     expect(span).to.deep.equal({});
+  });
+
+  it('should delegate setSpan to the delegate', () => {
+    proxyTraceManager.setDelegate(mockDelegate);
+    const context = {} as Context;
+    const span = {} as ExtendedSpan;
+    const resultContext = proxyTraceManager.setSpan(context, span);
+    expect(resultContext).to.deep.equal({});
+  });
+
+  it('should delegate getSpan to the delegate', () => {
+    proxyTraceManager.setDelegate(mockDelegate);
+    const context = {} as Context;
+    const span = proxyTraceManager.getSpan(context);
+    void expect(span).to.be.undefined;
   });
 });
