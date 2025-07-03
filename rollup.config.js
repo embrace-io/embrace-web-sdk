68f01043f9c0ac89dea4267ecb45d2fd693bad83
Jared Freeze <overbalance@users.noreply.github.com>
2025-07-03 13:06:55 -0500

    feat: explicitly export sdk to window (#511)

--- rollup.config.js
+++ rollup.config.js
@@ -113,8 +113,14 @@ export default defineConfig([
     output: {
       file: 'build/iife/bundle.js',
       format: 'iife',
-      name: 'EmbraceWebSdk', // Global variable name for the SDK
+      // global variable name for the SDK
+      name: 'EmbraceWebSdk',
       sourcemap: true,
+      // TODO create a new entry file that sets the window variable explicitly and remove this line.
+      // This is a workaround to assign the SDK to the global window object so users can lazyload the SDK.
+      // By default, rollup creates the export with 'var' and assumes the SDK is being used in a script tag,
+      // which would normally be the window.
+      footer: 'window.EmbraceWebSdk = EmbraceWebSdk;',
     },
     external: peerDeps,
     onwarn,
