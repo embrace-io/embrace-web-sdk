1887715955411fc4d5bc7ffba0374a73cff2553b
Joaquin Diaz <joaquin.diaz@embrace.io>
2025-07-08 14:28:23 -0300

    fix: enable black all tests, add debug logs again

--- tests/integration/tests/e2e/next-latest-tests.spec.ts
+++ tests/integration/tests/e2e/next-latest-tests.spec.ts
@@ -1,7 +1,7 @@
-// import { runE2ETests } from '../../utils/index.js';
-//
-// runE2ETests({
-//   name: 'Next Latest',
-//   url: 'http://localhost:3000',
-//   numberOfExpectedSpans: 14,
-// });
+import { runE2ETests } from '../../utils/index.js';
+
+runE2ETests({
+  name: 'Next Latest',
+  url: 'http://localhost:3000',
+  numberOfExpectedSpans: 14,
+});
--- tests/integration/tests/e2e/webpack-5-tests.spec.ts
+++ tests/integration/tests/e2e/webpack-5-tests.spec.ts
@@ -1,13 +1,13 @@
-// import { runE2ETests } from '../../utils/index.js';
-//
-// runE2ETests({
-//   name: 'Webpack 5 ESNext',
-//   url: 'http://localhost:3001/public/webpack-5/esnext/index.html',
-//   numberOfExpectedSpans: 3,
-// });
-//
-// runE2ETests({
-//   name: 'Webpack 5 ES2015',
-//   url: 'http://localhost:3001/public/webpack-5/es2015/index.html',
-//   numberOfExpectedSpans: 3,
-// });
+import { runE2ETests } from '../../utils/index.js';
+
+runE2ETests({
+  name: 'Webpack 5 ESNext',
+  url: 'http://localhost:3001/public/webpack-5/esnext/index.html',
+  numberOfExpectedSpans: 3,
+});
+
+runE2ETests({
+  name: 'Webpack 5 ES2015',
+  url: 'http://localhost:3001/public/webpack-5/es2015/index.html',
+  numberOfExpectedSpans: 3,
+});
