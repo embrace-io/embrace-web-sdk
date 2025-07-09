6de531f0e5900bf6c4dc58acda2d2285bf616fe4
overbalance <overbalance@users.noreply.github.com>
2025-07-09 15:00:46 -0500

    chore: add helper functions

--- demo/frontend/scripts/runDemo.sh
+++ demo/frontend/scripts/runDemo.sh
@@ -1,29 +1,34 @@
-# move to sdk directory (root)
-cd ../..
-#build sdk locally
-rm -rf node_modules
-npm ci
-rm -rf build
-npm run sdk:compile
-#build the cli locally
-cd ./cli || exit
-rm -rf build
-npm run cli:compile
-chmod +x build/index.js
-#build demo locally
-cd ../demo/frontend || exit
 # check if .env file exists
 if [ ! -f .env ]; then
   echo ".env file not found, create one from template and add app id"
   exit 1
 fi
+
+# clean workspaces
+npm run clean --workspace=@embrace-io/web-cli --prefix ../..
+npm run clean
+npm run clean --prefix ../..
+npm i
+
+# compile cli
+npm run compile --workspace=@embrace-io/web-cli --prefix ../..
+# macos needs permissions to execute the binary
+chmod +x ../../cli/build/index.js
+
+# compile sdk
+npm run compile --prefix ../..
+
+# compile the demo - you must install dependencies again to retrieve the cli package
+npm i
+npm run compile
+
 # add env vars from .env file to the current environment
 export $(grep -v '^#' .env | xargs)
-rm -rf build dist
-npm run demo:frontend:compile
-#find the path for the generated bundle
+
+# find the path for the generated bundle
 bundle_path=$(find ./dist/assets -name "index*.js")
 source_map_path=$(find ./dist/assets -name "index*.js.map")
+
 # process the bundle to replace the bundle id. NOTE: we don't upload source maps on each run, to avoid spamming s3, so symbolication won't work
 # If you need to upload source maps for testing, remove the "--no-upload" flag
 npm run demo:frontend:upload:sourcemaps -- -a $VITE_APP_ID -b "$bundle_path" -m "$source_map_path" --no-upload
