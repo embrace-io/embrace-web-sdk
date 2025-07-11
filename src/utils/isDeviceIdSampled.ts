53988e984622deda37241239dd841469fce50e3c
Joaquin Diaz <joaquin.diaz@embrace.io>
2025-07-11 16:32:54 -0300

    feat: add function to sample a deviceId deterministically

---
+++ src/utils/isDeviceIdSampled.ts
@@ -0,0 +1,33 @@
+const DIGITS = 6;
+
+/**
+ * Determines whether a deviceId is sampled. This is achieved
+ * by taking a normalized hex value from the last 6 digits of the device ID, and comparing
+ * it against the enabled percentage. This ensures that devices are consistently in a given
+ * group for beta functionality.
+ *
+ * This can be used to:
+ * - Select sample devices for telemetry collection
+ * - Enable/disable features for a percentage of users
+ *
+ * The normalized device ID has 16^6 possibilities (roughly 1.6m) which should be sufficient
+ * granularity for our needs.
+ */
+export const isDeviceIdSampled = (deviceId: string, pctEnabled: number) => {
+  if (pctEnabled <= 0 || pctEnabled > 100) {
+    return false;
+  }
+
+  const normalizedDeviceId = getNormalizedDeviceId(deviceId);
+
+  return pctEnabled >= normalizedDeviceId;
+};
+
+const getNormalizedDeviceId = (deviceId: string): number => {
+  const finalChars = deviceId.slice(-DIGITS); // last 6 chars
+  const radix = 16;
+  const space = Math.pow(radix, DIGITS) - 1;
+  const value = parseInt(finalChars, radix);
+
+  return (value / space) * 100;
+};
