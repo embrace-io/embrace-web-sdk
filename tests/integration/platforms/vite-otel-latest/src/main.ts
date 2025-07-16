c1bdc93ade74725de35197e8c56253aa30891c26
overbalance <overbalance@users.noreply.github.com>
2025-07-16 10:33:58 -0500

    chore: move test

---
+++ tests/integration/platforms/vite-otel-latest/src/main.ts
@@ -0,0 +1,28 @@
+import { sdk } from '@embrace-io/web-sdk';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
+import { ConsoleLogRecordExporter } from '@opentelemetry/sdk-logs';
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base';
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+
+const provider = new WebTracerProvider();
+
+provider.register({
+  contextManager: new ZoneContextManager(),
+});
+
+registerInstrumentations({
+  instrumentations: [new DocumentLoadInstrumentation()],
+});
+
+sdk.initSDK({
+  appID: '',
+  appVersion: 'YOUR_APP_VERSION',
+  logLevel: sdk.DiagLogLevel.INFO,
+  spanExporters: [new ConsoleSpanExporter()],
+  logExporters: [new ConsoleLogRecordExporter()],
+});
+
+const span = provider.getTracer('otel-collision').startSpan('main');
+span.end();
