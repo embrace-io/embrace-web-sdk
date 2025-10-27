fbffd2b555a35bd723159feb27d20d683bb223f5
overbalance <overbalance@users.noreply.github.com>
2025-10-27 14:04:56 -0500

    chore: remove comment

--- src/react-instrumentation/index.ts
+++ src/react-instrumentation/index.ts
@@ -1,6 +1,4 @@
 // Exposes all React-specific instrumentation in a way that is easy to tree-shake. Eventually this should be replaced by its own package.
-// That's why this rule doesn't apply here, and it will get fixed once we move this to its own package
-/* eslint-disable regex/invalid */
 
 import { EmbraceErrorBoundary } from '../instrumentations/exceptions/react/EmbraceErrorBoundary/index.js';
 import { createReactRouterNavigationInstrumentation } from '../instrumentations/navigation/NavigationInstrumentation/react/createReactRouterNavigationInstrumentation.js';
