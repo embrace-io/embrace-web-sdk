b4119eac98101446b2dca21231712c1d33e3de38
Jonathan Munz <jpmunz@gmail.com>
2025-10-29 12:00:03 -0400

    fix: allow null to be passed to the empty root instrumentation to account for not found elements (#938)

--- src/instrumentations/empty-root/EmptyRootInstrumentation/types.ts
+++ src/instrumentations/empty-root/EmptyRootInstrumentation/types.ts
@@ -1,6 +1,6 @@
 import type { EmbraceInstrumentationBaseArgs } from '../../EmbraceInstrumentationBase/index.js';
 
 export type EmptyRootInstrumentationArgs = {
-  rootNode: Node;
+  rootNode: Node | null;
   emptyCheckDelayMs?: number;
 } & Pick<EmbraceInstrumentationBaseArgs, 'diag' | 'perf'>;
