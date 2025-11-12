0bb2b7d4cd91ece51c13a26e2eb112c54b1fedd3
overbalance <overbalance@users.noreply.github.com>
2025-11-12 12:10:35 -0600

    chore: add next-16

---
+++ tests/integration/platforms/next-16/app/SDKTest.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { log, session } from '@embrace-io/web-sdk';
+import sdkControl from './otel';
+
+const SDKTest = () => {
+  const handleEndSession = async () => {
+    session.endSessionSpan();
+
+    if (sdkControl) {
+      await sdkControl.flush();
+    }
+  };
+
+  const handleSendLog = async () => {
+    log.message('This is a test log message', 'warning');
+
+    if (sdkControl) {
+      await sdkControl.flush();
+    }
+  };
+
+  const handleNavigateToAnotherPage = () => {
+    window.location.href = 'about:blank';
+  };
+
+  return (
+    <section>
+      <button onClick={handleEndSession}>End Session</button>
+      <button onClick={handleSendLog}>Send Log</button>
+      <button onClick={handleNavigateToAnotherPage}>
+        Navigate to Another Page
+      </button>
+    </section>
+  );
+};
+
+export default SDKTest;
