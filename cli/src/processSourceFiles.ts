b056f6f5ae5fba6a5ad7dc8347f0f0877635c9a9
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2026-01-14 13:59:50 -0300

    chore: do not inject embrace code to source files twice

--- cli/src/processSourceFiles.ts
+++ cli/src/processSourceFiles.ts
@@ -13,7 +13,7 @@ class MapSecurityError extends Error {
 
 // The un-minified version of FILE_BUNDLE_IDS_CODE_SNIPPET lives in cli/snippet/fileBundleIDsSnippet.js
 const FILE_BUNDLE_ID_CODE_SNIPPET_TEMPLATE = 'EmbIOFileBundleID';
-const FILE_BUNDLE_IDS_CODE_SNIPPET = `(E=>{try{const _=globalThis,c=(new _.Error).stack;c&&(_[E]=_[E]||{},_[E][c]="${FILE_BUNDLE_ID_CODE_SNIPPET_TEMPLATE}")}catch(E){}})("_EmbraceFileBundleIDs");`
+const FILE_BUNDLE_IDS_CODE_SNIPPET = `(E=>{try{const _=globalThis,c=(new _.Error).stack;c&&(_[E]=_[E]||{},_[E][c]="${FILE_BUNDLE_ID_CODE_SNIPPET_TEMPLATE}")}catch(E){}})("_EmbraceFileBundleIDs");`;
 const INJECTION_MARKER = '// Injected by Embrace Web CLI:';
 
 interface SourceMap {
