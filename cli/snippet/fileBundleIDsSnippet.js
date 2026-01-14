b6191f427843182121fdd50db7433742d830f82b
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2026-01-14 13:57:54 -0300

    chore: do not inject embrace code to source files twice

--- cli/snippet/fileBundleIDsSnippet.js
+++ cli/snippet/fileBundleIDsSnippet.js
@@ -1,7 +1,7 @@
 // This is the non-minified version of the code snippet that gets injected into JS files when running the cli tool.
 // This code is never executed by the sdk, but we keep it as source of truth for the minified version used in cli/src/processSourceFiles.ts
 // This code can be minified with: `npx terser ./cli/snippet/fileBundleIDsSnippet.js -c -m`
-((mapKey) => {
+((globalKey) => {
   try {
     const globalObj = globalThis;
 
@@ -10,10 +10,10 @@
 
     if (stack) {
       // Initialize the global map if it doesn't exist
-      globalObj[mapKey] = globalObj[mapKey] || {};
+      globalObj[globalKey] = globalObj[globalKey] || {};
 
       // Store a mapping of the stack trace to a placeholder (this is actually replaced in cli/src/processSourceFiles.ts)
-      globalObj[mapKey][stack] =
+      globalObj[globalKey][stack] =
         // biome-ignore lint/suspicious/noTemplateCurlyInString: template string is replaced during build process
         '${FILE_BUNDLE_ID_CODE_SNIPPET_TEMPLATE}';
     }
