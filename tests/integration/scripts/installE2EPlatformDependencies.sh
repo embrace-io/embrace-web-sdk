354921d288e21f1c9840b70755dc02c7a1693bdd
overbalance <overbalance@users.noreply.github.com>
2026-03-11 12:27:16 -0500

    build: use bun integration

--- tests/integration/scripts/installE2EPlatformDependencies.sh
+++ tests/integration/scripts/installE2EPlatformDependencies.sh
@@ -1,5 +1,5 @@
 find ./platforms -mindepth 2 -maxdepth 2 -type f -name package.json | while read pkg; do
   dir=$(dirname "$pkg")
   echo "Installing dependencies in $dir"
-  npm install --prefix "$dir"
+  npx -y bun install --no-save --cwd "$dir"
 done
