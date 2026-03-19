1303b8749705e071e545bf84f1c83bfb16ba8079
overbalance <overbalance@users.noreply.github.com>
2026-03-19 12:47:24 -0500

    chore: add dupe validation

--- packages/web-sdk/tsdown.config.ts
+++ packages/web-sdk/tsdown.config.ts
@@ -26,10 +26,11 @@ export default defineConfig([
     clean: false,
     plugins: [
       Sonda({
-        enabled: !!process.env.SONDA,
-        format: ['html', 'json'],
-        open: false,
+        enabled: true,
+        format: process.env.SONDA ? ['html', 'json'] : ['json'],
+        open: process.env.SONDA ? 'html' : false,
         gzip: true,
+        deep: true,
       }),
       failOnWarnPlugin,
     ],
