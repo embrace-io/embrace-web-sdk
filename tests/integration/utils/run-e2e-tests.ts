0fdd89497ff2b217f83eed99a16488e2ff4ae0b6
Jonathan Munz <jpmunz@gmail.com>
2026-06-19 13:00:59 -0400

    EMBR-12526 update goldens

--- tests/integration/utils/run-e2e-tests.ts
+++ tests/integration/utils/run-e2e-tests.ts
@@ -432,11 +432,14 @@ const runE2ETests = ({
         await page.getByRole('button', { name: 'End Session' }).click();
         await waitForOTelRequest();
 
-        testE2E.expect(requests).toHaveLength(2);
+        testE2E.expect(requests).toHaveLength(3);
         // Should contain a span capturing the fetch request
         if (goldenFiles) {
           extendedMockApiTestExpect(requests[1]).toMatchGoldenFile(
             `${browserName}-${codifiedName}-handle-204-with-body-session.json`,
+          );
+          extendedMockApiTestExpect(requests[2]).toMatchGoldenFile(
+            `${browserName}-${codifiedName}-handle-204-with-body-logs-after-part.json`,
           );
         }
       },
