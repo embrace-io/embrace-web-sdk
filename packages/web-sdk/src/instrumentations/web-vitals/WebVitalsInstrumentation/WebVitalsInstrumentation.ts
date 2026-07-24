a32d76ba69816d26f288d388e56538920e167dea
Joaquin Diaz <32420835+joaquin-diaz@users.noreply.github.com>
2026-07-24 16:16:57 -0300

    feat: capture INP element_type attribution attribute (#1445)

--- packages/web-sdk/src/instrumentations/web-vitals/WebVitalsInstrumentation/WebVitalsInstrumentation.ts
+++ packages/web-sdk/src/instrumentations/web-vitals/WebVitalsInstrumentation/WebVitalsInstrumentation.ts
@@ -132,6 +132,33 @@ const loafScriptsAttribution = (
   return attributes;
 };
 
+const inpAttribution = (
+  metric: MetricWithAttribution,
+  diag: DiagLogger,
+): Attributes => {
+  const attributes: Attributes = {};
+
+  try {
+    /* eslint-disable baseline-js/use-baseline */
+    const target = (metric.entries as PerformanceEventTiming[]).find(
+      (entry) => entry.target,
+    )?.target as Element | null | undefined;
+
+    const tagName = target?.tagName?.toLowerCase();
+
+    if (tagName) {
+      attributes[`${KEY_EMB_WEB_VITAL_ATTRIBUTION_PREFIX}element_type`] =
+        tagName;
+    }
+
+    /* eslint-enable baseline-js/use-baseline */
+  } catch (e) {
+    diag.error('error building INP element type attribution', e);
+  }
+
+  return attributes;
+};
+
 const ttfbSubPartsAttribution = (
   metric: MetricWithAttribution,
   diag: DiagLogger,
@@ -445,6 +472,7 @@ export class WebVitalsInstrumentation extends EmbraceInstrumentationBase {
         ...(metric.name === 'INP'
           ? loafScriptsAttribution(metric, this._diag)
           : {}),
+        ...(metric.name === 'INP' ? inpAttribution(metric, this._diag) : {}),
         ...(metric.name === 'TTFB'
           ? ttfbSubPartsAttribution(metric, this._diag)
           : {}),
