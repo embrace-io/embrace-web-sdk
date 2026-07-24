c1eeef5074d90b92b2ad9eaba0925369a431f553
overbalance <overbalance@users.noreply.github.com>
2026-07-24 15:28:52 -0700

    Revert "consolidate"
    This reverts commit b3787bdfa7c7bf1bd7ec3c9415f1c8595d4c7d40.

--- packages/web-sdk/src/instrumentations/web-vitals/WebVitalsInstrumentation/WebVitalsInstrumentation.ts
+++ packages/web-sdk/src/instrumentations/web-vitals/WebVitalsInstrumentation/WebVitalsInstrumentation.ts
@@ -511,11 +511,9 @@ export class WebVitalsInstrumentation extends EmbraceInstrumentationBase {
             }
           : {}),
         ...(metric.name === 'INP'
-          ? {
-              ...loafScriptsAttribution(metric, this._diag),
-              ...inpAttribution(metric, this._diag),
-            }
+          ? loafScriptsAttribution(metric, this._diag)
           : {}),
+        ...(metric.name === 'INP' ? inpAttribution(metric, this._diag) : {}),
         ...(metric.name === 'TTFB'
           ? ttfbSubPartsAttribution(metric, this._diag)
           : {}),
