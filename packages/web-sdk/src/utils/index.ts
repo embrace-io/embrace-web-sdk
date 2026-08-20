2b67a2274ae4bc717631bfb7ba23d3d7f74570d4
overbalance <overbalance@users.noreply.github.com>
2026-08-20 09:32:55 -0700

    feat(sdk): record SDK load and init timestamps on session parts

--- packages/web-sdk/src/utils/index.ts
+++ packages/web-sdk/src/utils/index.ts
@@ -29,6 +29,7 @@ export {
   createPerformanceObserver,
   isEntryTypeSupported,
 } from './performanceObserver/index.ts';
+export { SDK_LOAD_ORIGIN_OFFSET_MILLIS } from './sdkLoadTime.ts';
 export {
   isNetworkSpan,
   isSessionPartSpan,
