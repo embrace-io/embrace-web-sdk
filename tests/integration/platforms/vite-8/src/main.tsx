e08869ce719b48be3a282e5db45bf269f3e543b1
Jonathan Munz <jpmunz@gmail.com>
2026-09-18 17:04:45 -0400

    EMBR-13227 fix lint error

--- tests/integration/platforms/vite-8/src/main.tsx
+++ tests/integration/platforms/vite-8/src/main.tsx
@@ -1,5 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 import SDKTest from './SDKTest.tsx';
 
@@ -21,4 +21,4 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
