47a57ada70438ad53287674f76ca2cca903a5527
Joaquin Diaz <32420835+joaquin-diaz@users.noreply.github.com>
2026-09-24 13:18:15 -0300

    chore: add npm and playwright proxy instructions and propagate to docker (#1545)

--- tests/integration/tests/e2e/next-15-turbopack-app-tests.spec.ts
+++ tests/integration/tests/e2e/next-15-turbopack-app-tests.spec.ts
@@ -3,5 +3,5 @@ import { runE2ETests } from '../../utils/index.ts';
 runE2ETests({
   name: 'Next 15 Turbopack App',
   url: 'http://localhost:3010',
-  numberOfExpectedSpans: 12,
+  numberOfExpectedSpans: 11,
 });
--- tests/integration/tests/e2e/next-16-app-tests.spec.ts
+++ tests/integration/tests/e2e/next-16-app-tests.spec.ts
@@ -3,5 +3,5 @@ import { runE2ETests } from '../../utils/index.ts';
 runE2ETests({
   name: 'Next 16 App',
   url: 'http://localhost:3014',
-  numberOfExpectedSpans: 12,
+  numberOfExpectedSpans: 11,
 });
